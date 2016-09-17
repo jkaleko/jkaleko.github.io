7f41e215f966c78d2af002d1eaceb6aac09c4305
jkaleko <jason.kaleko@gmail.com>
2016-09-17 12:53:47 -0700

    Update bootstrap.js

--- vendor/bootstrap/js/bootstrap.js
+++ vendor/bootstrap/js/bootstrap.js
@@ -981,8 +981,12 @@ if (typeof jQuery === 'undefined') {
     this.escape()
     this.resize()
 
-    this.$element.on('click.dismiss.bs.modal', '[data-dismiss="modal"]', $.proxy(this.hide, this));
-
+    this.$element.on('click.dismiss.bs.modal', '[data-dismiss="modal"]', function () {
+      $.proxy(this.hide, this);
+      $('iframe').attr("src", jQuery("iframe").attr("src"));
+    });
+    
+    
     this.$dialog.on('mousedown.dismiss.bs.modal', function () {
       that.$element.one('mouseup.dismiss.bs.modal', function (e) {
         if ($(e.target).is(that.$element)) that.ignoreBackdropClick = true
