75c7cf7479c155173089eea04e0e2dd1810f716e
jkaleko <jason.kaleko@gmail.com>
2016-09-17 13:34:50 -0700

    Update bootstrap.js

--- vendor/bootstrap/js/bootstrap.js
+++ vendor/bootstrap/js/bootstrap.js
@@ -981,10 +981,8 @@ if (typeof jQuery === 'undefined') {
     this.escape()
     this.resize()
 
-    this.$element.on('click.dismiss.bs.modal', '[data-dismiss="modal"]', function () {
-        $.proxy(that.hide, that)
-        $('iframe').attr("src", jQuery("iframe").attr("src"))
-    })
+    this.$element.on('click.dismiss.bs.modal', '[data-dismiss="modal"]', $.proxy(that.hide, that))
+    this.on('hidden.bs.modal', $('iframe').attr("src", jQuery("iframe").attr("src")))
     
     this.$dialog.on('mousedown.dismiss.bs.modal', function () {
       that.$element.one('mouseup.dismiss.bs.modal', function (e) {
