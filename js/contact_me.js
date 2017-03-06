b7ea33f3ad841f9c70d2810a86ad195af79783eb
jkaleko <jason.kaleko@gmail.com>
2017-03-06 14:35:55 -0800

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://jkaleko.byethost5.com/contact_me.php",
+                url: "http://jkaleko.epizy.com/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
